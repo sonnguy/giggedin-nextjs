80c989bfd3c700ec2dfac9d77a378bf54ea32ae8
Son Nguy <nguynhuhuyson@gmail.com>
2020-09-22 10:10:17 +0700

    Update code

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,4 +1,4 @@
-import Document, { Head, Main, NextScript } from "next/document"
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { readFileSync } from "fs"
 import { join } from "path"
 
@@ -24,16 +24,25 @@ class InlineStylesHead extends Head {
     }
 }
 
-export default class CustomDocument extends Document {
+
+class CustomDocument extends Document {
+    static async getInitialProps(ctx) {
+        const initialProps = await Document.getInitialProps(ctx)
+        return { ...initialProps }
+    }
+
     render() {
         return (
-            <html>
+            <Html>
+                <Head />
                 <InlineStylesHead />
                 <body>
                     <Main />
                     <NextScript />
                 </body>
-            </html>
+            </Html>
         )
     }
 }
+
+export default CustomDocument
